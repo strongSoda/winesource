96806ff375bd666ca73980dce4d21c591f3269f5
strongsoda <strongsoda2@gmail.com>
2021-04-10 12:56:16 +0530

    update routes

--- src/global/constants/routes.ts
+++ src/global/constants/routes.ts
@@ -1,12 +1,12 @@
 const ROUTES = {
-        USER_SIGNUP: "/register",
-        USER_LOGIN: "/login",
-        ADMIN_SIGNUP: "/admin/register", 
-        ADMIN_LOGIN: "/admin/login",
-        DISCOVER: "/discover",
-        ADMIN_DASHBOARD: "/admin/dashboard",
-        PROFILE: "/profile",
-        VERIFY_OTP: "/otp/verify",
+        "USER_SIGNUP": "/register",
+        "USER_LOGIN": "/login",
+        "ADMIN_SIGNUP": "/admin/register", 
+        "ADMIN_LOGIN": "/admin/login",
+        "DISCOVER": "/discover",
+        "ADMIN_DASHBOARD": "/admin/dashboard",
+        "PROFILE": "/profile",
+        "VERIFY_OTP": "/otp/verify",
     }
 
 export default ROUTES
