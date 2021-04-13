297e46f9c24009cea4a4b745a01cac43c685c6d8
strongsoda <strongsoda2@gmail.com>
2021-04-13 15:07:17 +0530

    global css variables

---
+++ src/global/constants/css/variables.ts
@@ -0,0 +1,12 @@
+const CSSVARIABLES = {
+    primaryColor: "#000",
+    primaryColor2: '#fff',
+    primaryBackground: "#fff",
+    primaryBackgroundDark: "#eff4fb",
+    secondaryColor: "#890004",
+    secondaryBackground: "#890004",
+    secondaryBackground2: "#000",
+    primaryFontFamily: "Montserrat"
+}
+
+export default CSSVARIABLES
