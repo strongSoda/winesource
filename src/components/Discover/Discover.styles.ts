d50c8c1926ebaa05b0a1177e034f52736362b498
strongsoda <strongsoda2@gmail.com>
2021-04-17 10:17:33 +0530

    discover page

--- src/components/Discover/Discover.styles.ts
+++ src/components/Discover/Discover.styles.ts
@@ -1,10 +1,91 @@
 import { motion } from 'framer-motion';
+import CSSVARIABLES from 'global/constants/css/variables';
 import styled from 'styled-components';
 
 const DiscoverWrapper = styled.div`
     text-align: center;
     /* background-color: blue; */
     margin-top: 50px;
+    /* background-color: blue; */
+
+    .content {
+        margin-top: 10vh;
+
+        input {
+            width: 30%;
+            padding: 1em 1em;
+            background-color: ${CSSVARIABLES.primaryBackgroundDark};
+            border: none;
+            font-family: ${CSSVARIABLES.primaryFontFamily};
+            ::placeholder {
+                /* color: #cec5c5; */
+                font-family: ${CSSVARIABLES.primaryFontFamily};
+                font-weight: 500;
+                opacity: .5;
+            }
+        }
+
+        button {
+                            margin: 0 auto;
+                background-color: ${CSSVARIABLES.secondaryBackground};
+                color: ${CSSVARIABLES.primaryColor2};
+                border: none;
+                /* width: fit-content; */
+                padding: 1em 2em !important;
+                text-transform: uppercase;
+                margin-top: 4vh;
+                font-weight: 600;
+                cursor: pointer;
+        }
+
+        .category_deals, .daily_deals {
+            margin-top: 7vh;
+            margin-bottom: 8vh;
+        }
+        .wines {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: flex-start;
+            gap: 2em;
+            max-width: 60vw;
+            margin: 0 auto;
+
+            .wine {
+                margin-top: 4vh;
+                text-align: left;
+                img {
+                    height: 200px;
+                }
+                p {
+                    text-transform: capitalize;
+                }
+                .prices {
+                    display: flex;
+                    gap: 1em;
+                } 
+                .price {
+                    /* padding: 1em; */
+                    color: ${CSSVARIABLES.secondaryColor};
+                    font-weight: 600;
+                }
+                .avg_price {
+                    text-decoration: line-through;
+                    color: ${CSSVARIABLES.gray1}
+                }
+            }
+        }
+    }
+
+        @media (max-width: 768px) {
+            .wine {
+                margin: 0 auto;
+            }
+            input, button {
+                margin: 0 auto;
+                display: block;
+                width: 80% !important;
+            }
+        }
 `;
 
 export { DiscoverWrapper };
--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -1,27 +1,88 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { DiscoverWrapper } from './Discover.styles';
 
-import useLocalStorage from 'react-hook-uselocalstorage'
 import { Redirect, useHistory } from 'react-router';
 import { useAppDispatch, useAppSelector } from 'hooks/storeHooks';
 import { logoutUser } from 'features/counter/userSlice';
 import ROUTES from 'global/constants/routes';
+import Navbar from 'components/Navbar';
+import METHODS from 'global/constants/restMethods';
+import API from 'global/constants/api';
+import ENDPOINTS from 'global/constants/endpoints';
 
 declare interface IDiscoverProps {}
 
-const Discover: React.FC = (props: IDiscoverProps) => {  
-  const dispatch = useAppDispatch()
-  const loggedin = useAppSelector(state => state.user.loggedin)
-  const profile = useAppSelector(state => state.user.profile)
+const Discover: React.FC = (props: IDiscoverProps) => {    
+  const token = useAppSelector(state => state.user.token)
+  const [rows, setRows] = useState<any []>()
+  const [query, setQuery] = useState<string>()
+
+  useEffect(() => {
+              
+    async function getRows() {
+      const response = await fetch(API + ENDPOINTS.PRODUCTS, {
+            method: METHODS.GET,
+            headers: {
+              Authorization: 'Bearer ' + token
+            },
+          })
+      const data = await response.json()
+      console.log(data);
+      
+      setRows(data.body.products.slice(1,6))
+    }
+
+    getRows()
+    // setRows([])
+  }, [])
+
   return (
     <DiscoverWrapper data-testid="Discover">
-      <button onClick={() => dispatch(logoutUser())}>Logout</button>
-      <h3>Username: {profile?.username}</h3>
-      <p>Email: {profile?.email}</p>
-      <p>Name: {profile?.fname} {profile?.lname}</p>
-      <p>Date of birth: {profile?.dob}</p>
-      <p>Phone: {profile?.phone}</p>
+      <Navbar />
+      <section className="content">
+        <input type="text" placeholder="What are you looking for?" />
+        <button>Search</button>
+        {rows?.length ?
+          <>
+        <section className="daily_deals">
+            <h1>Daily Deals</h1>
+            <section className="wines">
+              {rows.map(row => (
+                <div className="wine" key={row.id}>
+                  <img src={row.img_url} alt={row.name}/>
+                  <p>{row.name}</p>
+                  <div className="prices">
+                    <span className="price">${parseFloat(row.winesource_price).toFixed(2)}</span>
+                    <span className="avg_price">${parseFloat(row.average_price).toFixed(2)}</span>
+                  </div>
+                </div>
+              ))}
+            </section>
+          </section>
+          
+          <section className="category_deals">
+            <h1>Category Deals</h1>
+            <section className="wines">
+              {rows.map(row => (
+                <div className="wine" key={row.id}>
+                  <img src={row.img_url} alt={row.name}/>
+                  <p>{row.name}</p>
+                  <div className="prices">
+                    <span className="price">${parseFloat(row.winesource_price).toFixed(2)}</span>
+                    <span className="avg_price">${parseFloat(row.average_price).toFixed(2)}</span>
+                  </div>
+                </div>
+              ))}
+            </section>
+        </section>
+          </>
+       :
+        <section>
+            <h1>No results found for {query}</h1>
+        </section>
+       }
+      </section>
     </DiscoverWrapper>
   )
 };
