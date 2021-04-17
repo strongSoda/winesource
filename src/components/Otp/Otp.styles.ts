85d8ac2643e23b265058426aec1a94977213c934
strongsoda <strongsoda2@gmail.com>
2021-04-17 10:18:30 +0530

    build otp page

--- src/components/Otp/Otp.styles.ts
+++ src/components/Otp/Otp.styles.ts
@@ -1,6 +1,170 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
+import CSSVARIABLES from 'global/constants/css/variables';
 
-const OtpWrapper = styled.div``;
+const OtpWrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+
+        header {
+            /* margin-top: 4vh; */
+            display: flex;
+            justify-content: space-around;
+            background-color: ${CSSVARIABLES.primaryBackground};
+            z-index: 10;
+            position: fixed;
+            height: fit-content;
+            width: 50%;
+            top: 0;
+            padding-top: 2vh;
+
+            .brand {
+                color: ${CSSVARIABLES.secondaryColor};
+                letter-spacing: 4px;
+                text-transform: uppercase;
+                font-weight: 600;
+            }
+        }
+
+    div {
+        width: 50%;
+        
+        section {
+            margin-top: 6vh;
+            display: flex;
+            justify-content: baseline;
+            padding-top: 5vh;
+            flex-direction: column;
+            height: 100%;
+            align-items: center;
+
+            button {
+                background: ${CSSVARIABLES.primaryColor};
+                color: ${CSSVARIABLES.primaryColor2};
+                padding: 1em 3em;
+                font-family: ${CSSVARIABLES.primaryFontFamily};
+                text-transform: uppercase;
+                cursor: pointer;
+                font-weight: 600;
+                border: none;
+            }
+
+            input {
+                width: 80%;
+                padding: 1.2em 1em;
+                background-color: ${CSSVARIABLES.primaryBackgroundDark};
+                border: none;
+                font-family: ${CSSVARIABLES.primaryFontFamily};
+                ::placeholder {
+                    /* color: #cec5c5; */
+                    font-family: ${CSSVARIABLES.primaryFontFamily};
+                    font-weight: 500;
+                    opacity: .5;
+                }
+                margin-bottom: 2vh;
+            }
+
+            div {
+                color: ${CSSVARIABLES.secondaryColor};
+                /* margin-left: 6em; */
+                font-size: .8em;
+            }
+
+            input[type=submit] {
+                background-color: ${CSSVARIABLES.secondaryBackground};
+                color: ${CSSVARIABLES.primaryColor2};
+                border: none;
+                width: fit-content;
+                padding: 1em 4em;
+                text-transform: uppercase;
+                margin-top: 3vh;
+                font-weight: 600;
+                cursor: pointer;
+            }
+        }
+    }
+
+
+    .banner {
+        background: linear-gradient(0deg, rgba(195, 20, 50, 0.7), rgba(36, 11, 54, 0.8)), url('https://images.pexels.com/photos/2308939/pexels-photo-2308939.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+
+        h1, p {
+        color: ${CSSVARIABLES.primaryColor2};
+        position: relative;
+        top: 30%;
+        width: 60%;
+        margin: 0 auto;
+        }
+
+        p {
+            margin-top: 2.5vh;
+        }
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+
+        header {
+            width: 100%;
+        }
+
+        overflow-y: auto;
+
+
+        .banner {
+            order: 1;
+            width: 100%;
+            height: fit-content;
+            padding-top: 2em;
+            padding-bottom: 2em;
+            margin-top: 10vh;
+
+            h1 {
+                text-align: center;
+                font-size: 1.2em;
+                font-weight: 600;
+            }
+            p {
+                font-size: .9em;
+            }
+            h1, p {
+                width: 80%;
+                top: 2%;
+            }
+        }
+        .form__wrapper {
+            width: 100%;
+            order: 2;
+            margin-bottom: 4vh;
+
+            section {
+                margin-top: 0vh;
+                padding-top: 0vh;
+
+                input {
+                    width: 80%;
+                }
+
+                & .address-search {
+                    width: 80% !important;
+                }
+
+                & .address-search {
+                    width: 80% !important;
+                }
+
+                & div {
+                    margin-left: 6vw;
+                    align-self: flex-start;
+                }
+            }
+        }
+    }
+`;
 
 export { OtpWrapper };
--- src/components/Otp/Otp.tsx
+++ src/components/Otp/Otp.tsx
@@ -1,13 +1,133 @@
-import React from 'react';
+import API from 'global/constants/api';
+import ENDPOINTS from 'global/constants/endpoints';
+import METHODS from 'global/constants/restMethods';
+import ROUTES from 'global/constants/routes';
+import { useAppSelector } from 'hooks/storeHooks';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 
 import { OtpWrapper } from './Otp.styles';
 
 declare interface IOtpProps {}
 
-const OTP: React.FC = (props: IOtpProps) => (
-  <OtpWrapper data-testid="Otp">
-    <span>Otp Component</span>
-  </OtpWrapper>
-);
+const OTP: React.FC = (props: IOtpProps) => {
+  const [Input, setInput] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
+  const [OtpSent, setOtpSent] = useState<boolean>(false)
+  const [OtpVerified, setOtpVerified] = useState<boolean>(false)
+  const profile = useAppSelector(state => state.user.profile)
+  const token = useAppSelector(state => state.user.token)
+  const [Error, setCustomError] = useState('')
+
+  const history = useHistory()
+
+  const sendOtp = async () => {
+    setCustomError('')
+    setLoading(true)
+    try {
+      const response = await fetch(API + ENDPOINTS.SEND_OTP, {
+        method: METHODS.POST,
+        headers: {
+          // @ts-ignore
+          Authorization: 'Bearer ' + token
+        }
+      })
+      setLoading(false)
+      const data = await response.json()
+  
+      if (data.status === 'SUCCESS') {
+        setOtpSent(true)
+      } else {
+        setCustomError(data.message)
+      }
+    }
+    catch (e) {
+      setCustomError(e)
+      console.error(e)
+    }
+  }
+
+  const verifyOtp = async () => {
+    setCustomError('')
+    setLoading(true)
+
+    try {
+      const response = await fetch(API + ENDPOINTS.VERIFY_OTP, {
+        method: METHODS.POST,
+        headers: {
+          // @ts-ignore
+          Authorization: 'Bearer ' + token
+        },
+        body: JSON.stringify({"otp": Input})
+      })
+  
+      setLoading(false)
+      const data = await response.json()
+  
+      if (data.status === 'SUCCESS') {
+        setOtpVerified(true)
+      } else {
+        setOtpSent(false)
+        setCustomError(data.message)
+      }
+    }
+    catch (e) {
+      // setCustomError(e.)
+      console.error(e)
+    }
+  }
+
+  return (
+    <OtpWrapper data-testid="Otp">
+      <div className="form__wrapper">
+        <header>
+          <h3 className="brand">wine source</h3>
+        </header>
+        <section>
+          {OtpSent ?
+            <>
+              {!OtpVerified ?
+                <section>
+                  <h2>Verify Account</h2>
+                  <div>{Error}</div>
+                  <input
+                  // type="email"
+                  name="email"
+                  id="email"
+                  onChange={(e) => setInput(e.target.value)}
+                  value={Input}
+                  placeholder="123456"
+                  required
+                  />
+                  <button onClick={verifyOtp}>{!loading?'Verify':'loading...'}</button>
+                </section>
+                :
+              <section>
+                  <h2>OTP Verified</h2>
+                  {profile.is_admin === 'True' ? 
+                    <button onClick={() => history.push(ROUTES.ADMIN_DASHBOARD)}>Go to site</button>
+                    :
+                    <button onClick={() => history.push(ROUTES.DISCOVER)}>Go to site</button>
+                  }
+              </section>
+              }
+            </>
+            :
+            <section>
+              <h2>Verify Account</h2>
+              <div>{Error}</div>
+              <p>Send OTP to {profile.phone}</p>
+              <button onClick={sendOtp}>{!loading?'Send':'loading...'}</button>
+            </section>
+          }
+        </section>
+      </div>
+      <div className="banner">
+        <h1>Welcome to WineSource</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem optio, error ratione saepe provident fugit nam dolorum temporibus iusto, id quas quo assumenda minus facere perspiciatis quaerat minima modi cum!</p>
+      </div>
+    </OtpWrapper>
+  )
+};
 
 export default OTP;
