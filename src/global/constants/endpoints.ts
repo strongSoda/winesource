435e77e78170b1f0acadd263a45960c2350e3155
strongsoda <strongsoda2@gmail.com>
2021-04-17 10:19:45 +0530

    new api endpoints

--- src/global/constants/endpoints.ts
+++ src/global/constants/endpoints.ts
@@ -2,7 +2,10 @@ const ENDPOINTS = {
     REGISTER: '/register',
     LOGIN: '/login',
     LOGOUT: '/logout',
-    USER: '/user'
+    USER: '/user',
+    SEND_OTP: '/otp/send',
+    VERIFY_OTP: '/otp/verify',
+    PRODUCTS: '/products',
 }
 
 export default ENDPOINTS
