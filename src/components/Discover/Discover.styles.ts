3886a167471636ad98bcfceccfd79f3db83476c9
strongsoda <strongsoda2@gmail.com>
2021-04-23 10:29:08 +0530

    update discover page styles

--- src/components/Discover/Discover.styles.ts
+++ src/components/Discover/Discover.styles.ts
@@ -5,38 +5,46 @@ import styled from 'styled-components';
 const DiscoverWrapper = styled.div`
     text-align: center;
     /* background-color: blue; */
-    margin-top: 50px;
+    margin-top: 0px;
     /* background-color: blue; */
+    .search__container {
+        width: 100vw;
+        margin-top: 45px;
+        position: fixed;
+        background-color: ${CSSVARIABLES.primaryBackground};
+        /* background-color: blue; */
+        /* left: 50%; */
+    }
+    input {
+        width: 30%;
+        padding: 1em 1em;
+        background-color: ${CSSVARIABLES.primaryBackgroundDark};
+        border: none;
+        font-family: ${CSSVARIABLES.primaryFontFamily};
+        ::placeholder {
+            /* color: #cec5c5; */
+            font-family: ${CSSVARIABLES.primaryFontFamily};
+            font-weight: 500;
+            opacity: .5;
+        }
+    }
+
+    button {
+                        margin: 0 auto;
+            background-color: ${CSSVARIABLES.secondaryBackground};
+            color: ${CSSVARIABLES.primaryColor2};
+            border: none;
+            /* width: fit-content; */
+            padding: 1em 2em !important;
+            text-transform: uppercase;
+            margin-top: 4vh;
+            font-weight: 600;
+            cursor: pointer;
+    }
 
     .content {
-        margin-top: 10vh;
+        padding-top: 10vh;
 
-        input {
-            width: 30%;
-            padding: 1em 1em;
-            background-color: ${CSSVARIABLES.primaryBackgroundDark};
-            border: none;
-            font-family: ${CSSVARIABLES.primaryFontFamily};
-            ::placeholder {
-                /* color: #cec5c5; */
-                font-family: ${CSSVARIABLES.primaryFontFamily};
-                font-weight: 500;
-                opacity: .5;
-            }
-        }
-
-        button {
-                            margin: 0 auto;
-                background-color: ${CSSVARIABLES.secondaryBackground};
-                color: ${CSSVARIABLES.primaryColor2};
-                border: none;
-                /* width: fit-content; */
-                padding: 1em 2em !important;
-                text-transform: uppercase;
-                margin-top: 4vh;
-                font-weight: 600;
-                cursor: pointer;
-        }
 
         .category_deals, .daily_deals {
             margin-top: 7vh;
@@ -53,6 +61,8 @@ const DiscoverWrapper = styled.div`
             .wine {
                 margin-top: 4vh;
                 text-align: left;
+                cursor: pointer;
+                
                 img {
                     height: 200px;
                 }
@@ -84,6 +94,7 @@ const DiscoverWrapper = styled.div`
                 margin: 0 auto;
                 display: block;
                 width: 80% !important;
+                margin-top: 2vh;
             }
         }
 `;
