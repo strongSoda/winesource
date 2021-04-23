2c5fcef8d2916777935d953eab2f5eb2ecc3765c
strongsoda <strongsoda2@gmail.com>
2021-04-23 10:29:53 +0530

    submit daily deals feature in inventory

--- src/components/Inventory/Inventory.styles.ts
+++ src/components/Inventory/Inventory.styles.ts
@@ -10,6 +10,32 @@ const InventoryWrapper = styled.div`
         margin: 0 auto;
         /* border: 2px solid black; */
     }
+
+    .actions {
+        /* background-color: blue; */
+        width: 50%;
+        /* display: inline; */
+        margin: 0 auto;
+        Button {
+            width: 30%;
+        }
+    }
+
+    .daily__deals {
+        position: fixed;
+        top: 5vh;
+        right: 0;
+        width: 20vw;
+        background-color: ${CSSVARIABLES.primaryBackground};
+        height: 100vh;
+        padding: 3em;
+
+        .actions {
+            Button {
+                width: 100%;
+            }
+        }
+    }
     }
 `;
 
--- src/components/Inventory/Inventory.tsx
+++ src/components/Inventory/Inventory.tsx
@@ -11,14 +11,17 @@ import ENDPOINTS from 'global/constants/endpoints';
 import METHODS from 'global/constants/restMethods';
 import { useAppSelector } from 'hooks/storeHooks';
 import Navbar from 'components/Navbar';
+import CSSVARIABLES from 'global/constants/css/variables';
+import Button from 'components/Button';
 
 declare interface IInventoryProps {}
 
 const Inventory: React.FC = (props: IInventoryProps) => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
+  const [showDailyDeals, setShowDailyDeals] = useState<boolean>(false);
   const token = useAppSelector(state => state.user.token)
-  
+  const [selectedRows, setSelectedRows] = useState<any []>([])
   const [rows, setRows] = useState()
 
   const defaultColDef = {
@@ -27,9 +30,21 @@ const Inventory: React.FC = (props: IInventoryProps) => {
     // make every column editable
     editable: true,
   };
+
+  const onGridReady = (params: any) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+  }
   
+  const onSelectionChanged = () => {
+    setShowDailyDeals(false)
+    // @ts-ignore
+    setSelectedRows(gridApi.getSelectedRows());
+  };
+
   const columnDefs = [
-    { headerName: "Name", field: "name", sortable: true, filter: true, resizable: true },
+    { headerName: "ID", field: "id", sortable: true, filter: true, resizable: true, width: 110},
+    { headerName: "Name", field: "name", sortable: true, filter: true, resizable: true, width: 220},
     { headerName: "Category", field: "category", sortable: true, filter: true, resizable: true },
     { headerName: "Vintage", field: "vintage", sortable: true, filter: true, resizable: true },
     { headerName: "Case Size", field: "case_size", sortable: true, filter: true, resizable: true },
@@ -48,7 +63,7 @@ const Inventory: React.FC = (props: IInventoryProps) => {
   useEffect(() => {
               
     async function getRows() {
-      const response = await fetch(API + ENDPOINTS.PRODUCTS, {
+      const response = await fetch(API + ENDPOINTS.SELLER_PRODUCTS, {
             method: METHODS.GET,
             headers: {
               Authorization: 'Bearer ' + token
@@ -67,18 +82,123 @@ const Inventory: React.FC = (props: IInventoryProps) => {
     <InventoryWrapper data-testid="Inventory">
       <Navbar />
       <section className="content">
+
+        <div className="actions">
+            <Button text="Daily Deals" color={CSSVARIABLES.primaryColor2} bgColor={CSSVARIABLES.secondaryBackground2} onClick={() => {setShowDailyDeals(!showDailyDeals)}} />
+            <Button text="Category Deals" color={CSSVARIABLES.primaryColor2} bgColor={CSSVARIABLES.secondaryBackground} onClick={() => {}}/>          
+            <Button text="Personal Deals" color={CSSVARIABLES.primaryColor2} bgColor={CSSVARIABLES.secondaryBackground2} onClick={() => {}}/>          
+        </div>
         <div className="ag-theme-material" style={{ width: '80%' }}>
           <AgGridReact
+            onGridReady={onGridReady}
             domLayout='autoHeight'
             defaultColDef={defaultColDef}
             columnDefs={columnDefs}
             rowData={rows}
+            rowSelection='multiple'
+            rowMultiSelectWithClick={true}
+            onSelectionChanged={onSelectionChanged}
           >
           </AgGridReact>
-          </div>
+        </div>
+        {
+          showDailyDeals && 
+        <DailyDeals wines={selectedRows}/>
+        }
       </section>
     </InventoryWrapper>
   )
 };
+
+interface DailyDealsProps {
+  wines: any []
+}
+
+const DailyDeals: React.FC<DailyDealsProps> = (props: DailyDealsProps) => {
+  const token = useAppSelector(state => state.user.token)
+  // const [deals, setDeals] = useState<any>([])
+  const [daily_deals, setDailyDeals] = useState<any []>([])
+    
+  const deals: any = props.wines.map(wine => wine.id)  
+
+
+  async function getDailyDeals() {
+    const response = await fetch(API + ENDPOINTS.SELLER_DEALS + '?deal_type=' + 'DAILY', {
+          method: METHODS.GET,
+          headers: {
+            Authorization: 'Bearer ' + token
+        },
+      })
+    const data = await response.json()
+    // console.log(data);
+    
+    setDailyDeals(data.body.deals)
+  }
+
+    useEffect(() => {
+    getDailyDeals()
+  }, [])
+
+  const submit = async () => {
+    if (!deals.length) return
+    const response = await fetch(API + ENDPOINTS.DEALS, {
+      method: METHODS.POST,
+      headers: {
+        Authorization: 'Bearer ' + token
+      },
+      body: JSON.stringify({deal_type: "DAILY", deals: deals})
+    })
+    const data = await response.json()
+
+      getDailyDeals()
+  }
+
+    const Delete = async (id: any) => {
+    const response = await fetch(API + ENDPOINTS.DEALS, {
+      method: METHODS.DELETE,
+      headers: {
+        Authorization: 'Bearer ' + token
+      },
+      body: JSON.stringify({product_id: id})
+    })
+      const data = await response.json()
+      
+      getDailyDeals()
+    }
+  
+  return (
+    <section className="daily__deals">
+      <h1>Submit Daily Deals</h1>
+      {props.wines.map(wine => (
+        <p>{wine.id}.){wine.name}</p>
+      ))}
+
+      <div className="actions">
+          <Button text="Submit" color={CSSVARIABLES.primaryColor2} bgColor={CSSVARIABLES.secondaryBackground2} onClick={submit} />
+      </div>
+
+      {daily_deals?.length ? 
+        <section className="submitted">
+          <h1>Submitted Daily Deals</h1>
+          <section className="wines">
+            {daily_deals.map(row => (
+              <div className="wine" key={row.id}>
+                {/* <img src={row.img_url} alt={row.name}/> */}
+                <p>{row.id}.) {row.name} <button onClick={() => Delete(row.id)}>delete</button></p>
+                {/* <div className="prices">
+                  <span className="price">${parseFloat(row.winesource_price).toFixed(2)}</span>
+                  <span className="avg_price">${parseFloat(row.average_price).toFixed(2)}</span>
+                </div> */}
+              </div>
+            ))}
+          </section>
+        </section>
+        :
+        ''
+      }
+
+    </section>
+  )
+}
 
 export default Inventory;
