7fc6b49883c2a05157200a0e322766e66a7ec337
strongsoda <strongsoda2@gmail.com>
2021-04-23 10:38:41 +0530

    update root reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import counterReducer from 'features/counter/counterSlice';
-import userSlice from 'features/counter/userSlice';
+import userSlice from 'features/userSlice';
+import orderSlice from 'features/orderSlice';
+import cartSlice from 'features/cartSlice';
 
-const rootReducer = combineReducers({ counter: counterReducer, user: userSlice.reducer});
+const rootReducer = combineReducers({ counter: counterReducer, user: userSlice.reducer, order: orderSlice.reducer, cart: cartSlice.reducer});
 
 export default rootReducer;
