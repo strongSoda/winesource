f5c9f6b400301e2b40505dbb44bd081ed7b52c89
strongsoda <strongsoda2@gmail.com>
2021-04-23 20:59:18 +0530

    redirect to register

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import App from 'components/App';
+import { Redirect } from 'react-router';
 
 const Home: React.FC = () => (
   <>
     <Helmet>
       <title>Home</title>
     </Helmet>
+    <Redirect to="/register"/>
     <App />
   </>
 );
