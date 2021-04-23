f4026efda43991ad10589ddbaaf1e32fc5b4684f
strongsoda <strongsoda2@gmail.com>
2021-04-23 10:22:41 +0530

    address search component

--- src/components/AddressSearch/AddressSearch.tsx
+++ src/components/AddressSearch/AddressSearch.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import { AddressSearchWrapper } from './AddressSearch.styles';
 
 declare interface IAddressSearchProps {
-  setUserAddress: (address: string, lat: number, lng: number, city: string, state: string, country: string)  => void
+  setUserAddress: (line1: string, line2: string, lat: number, lng: number, city: string, state: string, country: string, zip: string) => void,
+  setAddress: (address: string) => void
 }
 
   function getCountry(address_components: any) {
@@ -19,6 +20,34 @@ declare interface IAddressSearchProps {
       }
     }
   }
+  
+  function getLine1(address_components: any) {
+    for (let result in Object.values(address_components)) {
+      let obj = address_components[result]
+      // console.log(obj);
+      for (let key in obj) {
+        if (key === 'types') {
+          if (obj.types.includes("street_number")) {
+            return obj.long_name
+          }
+        }
+      }
+    }
+  }
+  
+    function getLine2(address_components: any) {
+    for (let result in Object.values(address_components)) {
+      let obj = address_components[result]
+      // console.log(obj);
+      for (let key in obj) {
+        if (key === 'types') {
+          if (obj.types.includes("street_number")) {
+            return obj.long_name
+          }
+        }
+      }
+    }
+    }
   
   function getCity(address_components: any) {
     for (let result in Object.values(address_components)) {
@@ -48,6 +77,20 @@ declare interface IAddressSearchProps {
     }
   }
   
+  function getZipCode(address_components: any) {
+    for (let result in Object.values(address_components)) {
+      let obj = address_components[result]
+      // console.log(obj);
+      for (let key in obj) {
+        if (key === 'types') {
+          if (obj.types.includes("postal_code")) {
+            return obj.long_name
+          }
+        }
+      }
+    }
+  }
+  
 let autoComplete: any;
     
 const loadScript = (url: string, callback: () => void) => {
@@ -70,25 +113,27 @@ const loadScript = (url: string, callback: () => void) => {
 };
   
 
-function handleScriptLoad(updateQuery: any, autoCompleteRef: any, setUserAddress: any) {
+function handleScriptLoad(updateQuery: any, autoCompleteRef: any, setUserAddress: any, setAddress: any) {
   autoComplete = new window.google.maps.places.Autocomplete(
     autoCompleteRef.current,
     { componentRestrictions: { country: "us" } }
   );
   autoComplete.setFields(["address_components", "formatted_address", "geometry"]);
   autoComplete.addListener("place_changed", () =>
-    handlePlaceSelect(updateQuery, setUserAddress)
+    handlePlaceSelect(updateQuery, setUserAddress, setAddress)
   );
 }
 
-async function handlePlaceSelect(updateQuery: any, setUserAddress: any) {
+async function handlePlaceSelect(updateQuery: any, setUserAddress: any, setAddress: any) {
   const addressObject = autoComplete.getPlace();
   // console.log(addressObject);
+  
   const query = addressObject.formatted_address;
   const latLng = addressObject.geometry.location
   const address_components = addressObject.address_components
   updateQuery(query);
-  setUserAddress(addressObject.formatted_address, latLng.lat(), latLng.lng(), getCity(address_components), getState(address_components), getCountry(address_components))
+  setUserAddress(getLine1(address_components), getLine2(address_components), latLng.lat(), latLng.lng(), getCity(address_components), getState(address_components), getCountry(address_components), getZipCode(address_components))
+  setAddress(addressObject.formatted_address)
 }
 
 
@@ -100,7 +145,7 @@ const AddressSearch: React.FC<IAddressSearchProps> = (props: IAddressSearchProps
     loadScript(
       'https://maps.googleapis.com/maps/api/js?key=AIzaSyBrdAUqv6MyTY5weOsIqNJZXgHNpCFJoBs&callback=initialize&libraries=places',
       // `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`,
-      () => handleScriptLoad(setAddress, autoCompleteRef, props.setUserAddress)
+      () => handleScriptLoad(setAddress, autoCompleteRef, props.setUserAddress, props.setAddress)
     );
   }, []);
 
