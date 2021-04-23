883971137611696e3c4c19560ee6a0415b429c2d
strongsoda <strongsoda2@gmail.com>
2021-04-23 10:28:51 +0530

    update discover page with search view & default view

--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -4,20 +4,25 @@ import { DiscoverWrapper } from './Discover.styles';
 
 import { Redirect, useHistory } from 'react-router';
 import { useAppDispatch, useAppSelector } from 'hooks/storeHooks';
-import { logoutUser } from 'features/counter/userSlice';
+import { logoutUser } from 'features/userSlice';
 import ROUTES from 'global/constants/routes';
 import Navbar from 'components/Navbar';
 import METHODS from 'global/constants/restMethods';
 import API from 'global/constants/api';
 import ENDPOINTS from 'global/constants/endpoints';
+import Searchview from 'components/Searchview';
+import DefaultView from 'components/DefaultView';
 
 declare interface IDiscoverProps {}
 
 const Discover: React.FC = (props: IDiscoverProps) => {    
   const token = useAppSelector(state => state.user.token)
-  const [rows, setRows] = useState<any []>()
-  const [query, setQuery] = useState<string>()
-
+  const [rows, setRows] = useState<any []>([])
+  const [searchRows, setSearchRows] = useState<any []>([])
+  const [query, setQuery] = useState<string>('')
+  const [searching, setSearching] = useState<boolean>(false)
+  const [searchView, setSearchView] = useState<boolean>(false)
+    
   useEffect(() => {
               
     async function getRows() {
@@ -28,60 +33,47 @@ const Discover: React.FC = (props: IDiscoverProps) => {
             },
           })
       const data = await response.json()
-      console.log(data);
+      // console.log(data);
       
-      setRows(data.body.products.slice(1,6))
+      setRows(data.body.products)
     }
 
     getRows()
     // setRows([])
   }, [])
 
+  const search = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (!query) return
+    setSearchView(false)
+    setSearching(true)
+    const response = await fetch(API + ENDPOINTS.PRODUCTS + '?query=' + query, {
+      method: METHODS.GET,
+      headers: {
+        Authorization: 'Bearer ' + token
+      },
+    })
+    const result = await response.json()
+    console.log(result);
+    setSearchRows(result.body.products);
+    setSearching(false)
+    setSearchView(true)
+  } 
+
   return (
     <DiscoverWrapper data-testid="Discover">
       <Navbar />
-      <section className="content">
-        <input type="text" placeholder="What are you looking for?" />
-        <button>Search</button>
-        {rows?.length ?
-          <>
-        <section className="daily_deals">
-            <h1>Daily Deals</h1>
-            <section className="wines">
-              {rows.map(row => (
-                <div className="wine" key={row.id}>
-                  <img src={row.img_url} alt={row.name}/>
-                  <p>{row.name}</p>
-                  <div className="prices">
-                    <span className="price">${parseFloat(row.winesource_price).toFixed(2)}</span>
-                    <span className="avg_price">${parseFloat(row.average_price).toFixed(2)}</span>
-                  </div>
-                </div>
-              ))}
-            </section>
-          </section>
+        <div className="search__container">
           
-          <section className="category_deals">
-            <h1>Category Deals</h1>
-            <section className="wines">
-              {rows.map(row => (
-                <div className="wine" key={row.id}>
-                  <img src={row.img_url} alt={row.name}/>
-                  <p>{row.name}</p>
-                  <div className="prices">
-                    <span className="price">${parseFloat(row.winesource_price).toFixed(2)}</span>
-                    <span className="avg_price">${parseFloat(row.average_price).toFixed(2)}</span>
-                  </div>
-                </div>
-              ))}
-            </section>
-        </section>
-          </>
-       :
-        <section>
-            <h1>No results found for {query}</h1>
-        </section>
-       }
+        <form onSubmit={(e) => search(e)}>
+          <input type="text" placeholder="What are you looking for?" value={query} onChange={(e) => setQuery(e.target.value)} />
+          <button type="submit">{searching?'Searching...':'Search'}</button>
+        </form>
+        </div>
+      <section className="content">
+
+        {searchView ? <Searchview query={query} rows={searchRows} setSearchView={setSearchView}/> : <DefaultView rows={rows}/>}
+
       </section>
     </DiscoverWrapper>
   )
